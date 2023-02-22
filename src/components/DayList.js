cf2dd4df44b3aa3437e8cdb9560dca2f335981f1
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-02-21 21:00:31 -0700

    Create a function to produce <DayListItem /> for each item in days array

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,10 +2,19 @@ import React from "react";
 import DayListItem from "components/DayListItem"
 
 export default function DayList(props) {
+  const days = props.days;
+
+  const dayListItems = days.map((day) => 
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  );
 
   return (
-    <ul>
-
-    </ul>
+    <ul>{dayListItems}</ul>
   );
 }
