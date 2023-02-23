bc4119583d7dde2586c911ea5bac3e60348c152c
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-02-22 17:37:18 -0700

    Create InterviewerList and List Item and refactored props

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+
+  const interviewerClass = classNames('interviewers__item', {
+    "interviewers__item--selected": props.selected
+  });
+  
+  const displayName = function(props) {
+    return (props.selected ? props.name : "");
+  };
+
+  return (
+    <li onClick={props.setInterviewer} className={interviewerClass}>
+      <img
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+      />
+      {displayName(props)}
+    </li>
+  );
+}
