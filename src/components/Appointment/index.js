4a40ce82f944387f50cf85475210408916a2a5a2
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-02-23 18:06:13 -0700

    Finish Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,25 @@
-import React from "react"; 
+import React, { Fragment } from "react"; 
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
+  const interview = props.interview;
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+        <Header time={props.time} />
+        <Fragment>
+          {interview ?
+          <>
+            <Show student={interview.student} interviewer={interview.interviewer} /> 
+          </>
+          :
+          <>
+            <Empty />
+          </>}
+        </Fragment>
+    </article>
   );
 }
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,15 +3,14 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-  const interviewers = props.interviewers;
   
-  const interviewerListItems = interviewers.map((interviewer) => 
+  const interviewerListItems = props.interviewers.map((interviewer) => 
     <InterviewerListItem
       key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
       selected={interviewer.id === props.value}
-      setInterviewer={() => props.onChange(interviewer.id)}  
+      setInterviewer={()=> {props.onChange(interviewer.id)}}  
     />
   );
 
